6f56adcd63ab84e29da38d8694717a56b09d8ca0
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-24 16:49:43 +0700

    Update B5_UNION.c

--- C PROGRAMMING LANGUAGE/B5_STRUCT_UNION/B5_UNION.c
+++ C PROGRAMMING LANGUAGE/B5_STRUCT_UNION/B5_UNION.c
@@ -2,14 +2,24 @@
 #include <stdint.h>
 
 typedef union{
-    uint8_t  x[13];
-    uint16_t y[15];
-    uint32_t z[10];
-    uint64_t k[3];      
+    uint8_t x;
+    uint16_t y[10];
+    uint64_t k;      
 }toaDo;
 
 int main(){
-  printf("size: %d\n", sizeof(toaDo));
+  printf("size: %lu\n", sizeof(toaDo));
   return 0;
 }
+
+
+
+/* lấy member có kiểu dữ liệu lớn nhất để quét
+1 ô nhớ 1 byte uint8_t x -> quét 1 8 byte (1 ô nhớ)             dư 7 byte đệm (không quan tâm) => tổng 8 byte
+1 ô nhớ 2 byte uint16_t y[10] -> quét 1 8 byte (4 ô nhớ)
+                                 quét 2 8 byte (4 ô nhớ)
+                                 quét 3 8 byte (2 ô nhớ)        dư 4 byte đệm (không quan tâm) => tổng 24 byte
+1 ô nhơ 8 byte uint64_t k -> quét 1 8 byte (1 ô nhớ)            => tổng 8 byte
+=> So sánh 3 member thì uint16_t y[10] lớn nhất nên kích thước bộ nhớ Union là 24 byte */
+
 
