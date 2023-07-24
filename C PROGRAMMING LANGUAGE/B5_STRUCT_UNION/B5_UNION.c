869d458d6862f8e772f49b9564099e474b24a5f9
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-24 16:39:39 +0700

    Update B5_UNION.c

--- C PROGRAMMING LANGUAGE/B5_STRUCT_UNION/B5_UNION.c
+++ C PROGRAMMING LANGUAGE/B5_STRUCT_UNION/B5_UNION.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+
+typedef union{
+    uint8_t  x[13];
+    uint16_t y[15];
+    uint32_t z[10];
+    uint64_t k[3];      
+}toaDo;
+
+int main(){
+  printf("size: %d\n", sizeof(toaDo));
+  return 0;
+}
 
