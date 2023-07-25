7fe96fceb1be99da8b1aeae816dba7776792136c
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-25 16:04:25 +0700

    Update B7.c

--- C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/B7.c
+++ C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/B7.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#define SUM(a,b) a+b 
+#define TEST abc
 
+int main(){
+    
+    printf("tong a va b = %d\n", SUM(3,6));
+    
+    TEST;
+
+    return 0;
+}
+//Macro định nghĩa gì thì nó sẽ thay thế vào code cái đó trong quá trình tiền xử lí
