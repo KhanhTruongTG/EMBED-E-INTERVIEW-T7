977522352d6b8bdf75c4d6d3b4e8b5c92e44b94f
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-25 09:08:14 +0700

    Update and rename B3.c to VD_tran_bo_nho.c

--- C PROGRAMMING LANGUAGE/B3_MEMORY_ALLOCATION/VD_tran_bo_nho.c
+++ C PROGRAMMING LANGUAGE/B3_MEMORY_ALLOCATION/VD_tran_bo_nho.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+
 //VD tran bo nho Stack
 int foo(int x){
  printf("De quy khong gioi han\n");
@@ -7,11 +8,7 @@ int foo(int x){
 }
 int main()
 {
-  //cap phat dong
-  uint8_t *ptr=malloc(5);
-  uint8_t *ptr=(uint8_t*)malloc(5);
-  ptr = (uint8_t *)realloc(ptr, 7 * sizeof(uint8_t));
-  free(ptr);
+  
   
   return 0;
 }
