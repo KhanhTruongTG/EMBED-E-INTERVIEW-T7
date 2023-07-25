7d9a8f366eb49793abf253b044549bd3675d48a9
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-25 09:00:21 +0700

    Create BSS.c

---
+++ C PROGRAMMING LANGUAGE/B3_MEMORY_ALLOCATION/BSS.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int a=0;        //khởi tạo bằng 0
+static int b;   //không khởi tạo giá trị
+
+//giá trị khởi tạo bằng 0 hoặc không có giá trị -> lưu trong vùng nhớ Bss
+int main(){
+    
+return 0;
+}
