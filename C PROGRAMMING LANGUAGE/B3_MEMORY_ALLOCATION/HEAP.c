eb6257ba78eb1f2804d77cf3986da16fe0fbe3d0
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-25 09:10:24 +0700

    Create HEAP.c

---
+++ C PROGRAMMING LANGUAGE/B3_MEMORY_ALLOCATION/HEAP.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+//cap phat dong
+  uint8_t *ptr=malloc(5);
+  uint8_t *ptr=(uint8_t*)malloc(5);
+  ptr = (uint8_t *)realloc(ptr, 7 * sizeof(uint8_t));
+  free(ptr);
+
+//cấp phát động tạo ra vùng nhớ -> lưu ở phân vùng nhớ Heap
+  return 0;
+}
