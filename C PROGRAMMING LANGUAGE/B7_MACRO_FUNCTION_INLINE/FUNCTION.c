036e1ef6231f0d4b2cfcf6b13694ab5cdafd3044
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-26 14:03:10 +0700

    Update FUNCTION.c

--- C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/FUNCTION.c
+++ C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/FUNCTION.c
@@ -17,7 +17,6 @@ int main()
     tong(4,5);
     return 0;
 }
-
 //chương trình chạy từ 0x01 đến 0x03 thì gặp Hàm, địa chỉ tiếp theo là 0x04 sẽ được Stack Poiter lưu lại, Program Couter sẽ lấy địa chỉ của Hàm để đếm tiếp 0xC1 đến 0xC9
 //sau khi chạy hết chương trình Program Couter vào Stack Poiter lấy địa chỉ đã lưu ban đầu vào chạy tiếp tức 0x04, Stack Poiter sẽ xóa giá trị 0x04
 //tiếp đó Program Couter sẽ đếm tiếp dến 0x06 gặp tiếp Hàm thì Stack Poiter sẽ lưu 0x07, Program Couter sẽ lấy địa chỉ của Hàm để đếm tiếp 0xC1 đến 0xC9
