11d097dff08bb2eb7b6938c145549e6f7436dd11
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-07-26 11:43:40 +0700

    Update FUNCTION.c

--- C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/FUNCTION.c
+++ C PROGRAMMING LANGUAGE/B7_MACRO_FUNCTION_INLINE/FUNCTION.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdint.h>
 void tong (int a, int b){
     printf("tong a va b: %d\n",a+b);
 }
@@ -10,8 +10,8 @@ int main()
     int b = 6;
     a = a-b;
 
-    tong(9,5);
-    tong(6,4);
+    tong(4,5);
+    tong(2,4);
 
     return 0;
 }
