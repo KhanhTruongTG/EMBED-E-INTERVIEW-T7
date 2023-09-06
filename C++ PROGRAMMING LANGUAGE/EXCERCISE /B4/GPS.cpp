9110202b4739706c94de895afaebd5587afb633d
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-09-06 21:59:30 +0700

    Update GPS.cpp

--- C++ PROGRAMMING LANGUAGE/EXCERCISE /B4/GPS.cpp
+++ C++ PROGRAMMING LANGUAGE/EXCERCISE /B4/GPS.cpp
@@ -38,11 +38,11 @@ double GPSPoint::toRadians(double degrees){
     return degrees * M_PI / 180.0;
 }
 
-double GPSPoint::distanceTo(const GPSPoint& other){
+double GPSPoint::distanceTo(const GPSPoint& point){
     double lat1 = toRadians(latitude);
-    double lat2 = toRadians(other.latitude);
-    double deltaLat = toRadians(other.latitude - latitude);
-    double deltaLon = toRadians(other.longitude - longitude);
+    double lat2 = toRadians(point.latitude);
+    double deltaLat = toRadians(point.latitude - latitude);
+    double deltaLon = toRadians(point.longitude - longitude);
     double a = sin(deltaLat / 2) * sin(deltaLat / 2) + cos(lat1) * cos(lat2) * sin(deltaLon / 2) * sin(deltaLon / 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return EARTH_RADIUS * c;
