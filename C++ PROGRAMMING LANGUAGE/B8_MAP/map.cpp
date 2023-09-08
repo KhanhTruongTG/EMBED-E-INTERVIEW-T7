956ed894d4b8f0796b0698a116e7b736a9db3635
KhanhTruongTG <139245069+KhanhTruongTG@users.noreply.github.com>
2023-09-08 21:03:58 +0700

    Update map.cpp

--- C++ PROGRAMMING LANGUAGE/B8_MAP/map.cpp
+++ C++ PROGRAMMING LANGUAGE/B8_MAP/map.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
 
+typedef struct{
+    string name;
+    int age;
+}Infor;
+
+
+int main(){
+    map<int, Infor> Student;
+    Student[100] = Infor{"Hoang", 20}; 
+    Student[101] = Infor{"Khanh", 24};
+    Student[102] = Infor{"Bien", 23};
+    for(auto item : Student){
+        cout << endl << "ID = " << item.first << ", Name: " << item.second.name << ", age: " << item.second.age << endl;
+    }
+    return 0;
+}
